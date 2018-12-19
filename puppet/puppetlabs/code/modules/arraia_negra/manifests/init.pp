48d8384211e4ee2438100f69ddeaabc17d97a379
Juliana Matos <juferreira@gmail.com>
2018-12-19 22:56:05 +0000

    Infra Agil Tudo Ai!

---
+++ puppet/puppetlabs/code/modules/arraia_negra/manifests/init.pp
@@ -0,0 +1,25 @@
+class arraia_negra {
+
+	package{"apache2":
+		ensure => absent,
+		require => Service["apache2"],
+	}
+
+	service{"apache2":
+		ensure => stopped,
+	}
+
+
+	package{"nginx":
+		ensure => absent,
+		require => Package["apache2"],
+	}
+
+	service{"nginx":
+		ensure => running,
+		enable => true,
+		require => Package["nginx"],
+	}
+
+
+}
